105ac054073885e8f93dfca7bb8d0b1406a8c1c6
asus <asus@LAPTOP-GQ2P7O3H>
2019-08-30 10:48:37 +0530

    change in question8 using string buffer to check wheather a string is pallindrom or not

--- src/com/worldpay/StringTask/Ques8.java
+++ src/com/worldpay/StringTask/Ques8.java
@@ -3,14 +3,14 @@ package com.worldpay.StringTask;
 import java.util.Scanner;
 
 public class Ques8 {
-	private static Scanner sc;
+	
 
 	// Pallindrom using stringbuffer
 	public static void main(String args[]) {
-		sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter a word:");
 		String original = sc.nextLine();
-		StringBuffer sb = new StringBuffer();
+		StringBuffer sb = new StringBuffer(original);
 		sb.reverse();
 		String Reverse = sb.toString();
 		if (original.equals(Reverse)) {
